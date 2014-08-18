be9b7e06570f7fe9d5c3958a68a261d93c01cc09
David Woolf <davidjohnwoolf@gmail.com>
2014-08-18 02:13:13 -0600

    add action functionality to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,18 +5,32 @@ class ProductsController < ApplicationController
   end
 
   def show
-
+    @product = Product.find(params[:id])
   end
 
   def search
-    
+
   end
 
   def results
-    # @brand = params[:brand]
-    SEMANTICS.products_field( "brand", params[:q] )
+    if request.post?
+      SEMANTICS.products_field( "brand", params[:q] )
+      @products_hash = SEMANTICS.get_products
 
-    @products_hash = SEMANTICS.get_products
+      @products_hash['results'].each do |result|
+        @product = Product.new
+        @product.brand = result['brand']
+        @product.category = result['category']
+        @product.manufacturer = result['manufacturer']
+        @product.name = result['name']
+        @product.price = result['price']
+        @product.upc = result['upc']
+        @product.save
+      end
+    else
+      SEMANTICS.products_field( "brand", params[:q] )
+      @products_hash = SEMANTICS.get_products
+    end
   end
 
 end
