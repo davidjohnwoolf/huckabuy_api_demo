54a328e41f4f83f9f963940327769d78fb8b2cab
David Woolf <davidjohnwoolf@gmail.com>
2014-08-18 00:07:21 -0600

    write products results action and view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,15 +5,18 @@ class ProductsController < ApplicationController
   end
 
   def show
-    
+
   end
 
   def search
-
+    
   end
 
   def results
+    # @brand = params[:brand]
+    SEMANTICS.products_field( "brand", params[:q] )
 
+    @products_hash = SEMANTICS.get_products
   end
 
 end
